1b5f3e8c953f6a154fb410c85532add0de5bc63d
Nick Bailey <nick@n-ism.org>
2012-01-01 16:59:24 +0000

    Added getopt stuff

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <libconfig.h++>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <getopt.h>
 #include <config.h>
 #include "SoundModelPoly.h"
 #include "PlayoutThread.h"
@@ -23,20 +24,22 @@
 using namespace std;
 using namespace libconfig;
 
-int main(int argc, char ** argv) {
+int usage(void);
 
-	if(argc > 1) {
-		cerr << "Usage: " << argv[0] << endl;
-		exit(1);
-	}
+int main(int argc, char ** argv) {
 
 	// Check to see whether configuration file exists
 	string cfg_path(getenv("HOME"));
         cfg_path += "/.smrgygurdy";
 	struct stat cfg_stat;
 	if (stat(cfg_path.c_str(), &cfg_stat) == -1) {
+		cerr << "Couldn't stat " << cfg_path <<
+		        " - looking in /etc" << endl;
+		cfg_path = "/etc/smrgygurdy.conf";
+	}
+	if (stat(cfg_path.c_str(), &cfg_stat) == -1) {
 		cerr << "Couldn't stat config file" << endl;
-		exit(-1); // Should maybe copy a standard one here?
+		exit(-1);
 	}
 	if (!(cfg_stat.st_mode & S_IRUSR)) {
 		cerr << "Cannot read from " << cfg_path <<
@@ -50,10 +53,72 @@ int main(int argc, char ** argv) {
 	string pcm;        cfg.lookupValue("pcm", pcm);
 	int bsize;         cfg.lookupValue("buffer_size", bsize);
 	int rate;          cfg.lookupValue("sample_rate", rate);
-	int poly;          cfg.lookupValue("max_voices", poly);
+	int poly;          cfg.lookupValue("polyphony", poly);
 	int noThreads;     cfg.lookupValue("threads", noThreads);
 	int keyboard_id;   cfg.lookupValue("midi.controller_id", keyboard_id);
 	int keyboard_port; cfg.lookupValue("midi.controller_port", keyboard_port);
+
+	// Parse command line, perhaps changing configured values
+	static struct option long_options[] =  {
+	  {"alsa-device", required_argument, 0, 'd'},
+	  {"buffer-size", required_argument, 0, 'b'},
+	  {"sample-rate", required_argument, 0, 'r'},
+	  {"polyphony",   required_argument, 0, 'p'},
+	  {"threads",     required_argument, 0, 't'},
+	  {"midi-id",     required_argument, 0, 'M'},
+	  {"midi-port",   required_argument, 0, 'm'},
+	  {"help",        no_argument,       0, 'h'},
+	  {0, 0, 0, 0}
+	};
+	int c, option_index;
+	do {
+		c = getopt_long (argc, argv,
+				 "d:b:r:p:t:M:m:h",
+		                 long_options, &option_index);
+		switch (c) {
+		    case 'd':
+			pcm = optarg;
+			cfg.lookup("pcm") = pcm;
+			cout << "Using alsa device \"" << pcm << "\"" << endl;
+			break;
+		    case 'b':
+			bsize = atoi(optarg);
+			cfg.lookup("buffer_size") = bsize;
+			cout << "Audio buffer size = " << bsize << endl;
+			break;
+		    case 'r':
+			rate = atoi(optarg);
+			cfg.lookup("sample_rate") = rate;
+			cout << "Audio sample rate = " << rate << endl;
+			break;
+		    case 'p':
+			poly = atoi(optarg);
+			cfg.lookup("polyphony") = poly;
+			cout << "Polyphony: " << poly << " voices" << endl;
+			break;
+		    case 't':
+			noThreads = atoi(optarg);
+			cfg.lookup("threads") = noThreads;
+			cout << noThreads << " concurrent threads" << endl;
+			break;
+		    case 'M':
+			keyboard_id = atoi(optarg);
+			cfg.lookup("midi.controller_id") = keyboard_id;
+			cout << "Midi controller has id " << keyboard_id << endl;
+			break;
+		    case 'm':
+			keyboard_port = atoi(optarg);
+			cfg.lookup("midi.controller_port") = keyboard_port;
+			cout << "Midi controller uses port " << keyboard_port << endl;
+			break;
+		    case 'h':
+			exit (usage());
+		    case '?':
+			// getopt_long already printed an error message
+			break;
+		}
+
+	} while (c != -1);
 
 	// Create Models
 	SoundModel *mainModel;			/* SoundModel to be passed to controller */
@@ -106,7 +171,7 @@ int main(int argc, char ** argv) {
 
 	cout << "Enter Q to quit" << endl;
 	cout << "      C to report stats" << endl;
-	int c;
+
 	while( (c = getchar()) != 'q') {
 	    switch (c) {
 	      case 'c':
@@ -127,5 +192,10 @@ int main(int argc, char ** argv) {
 	}
 	cfg.writeFile(cfg_path.c_str());
 	return 0;	
+}
+
+int usage(void)
+{
+	return 0;
 }
 
