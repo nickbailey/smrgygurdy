b2c874c6c6eb0d0626819c554742a95a59b4c35c
Nick Bailey <nick@n-ism.org>
2016-05-26 18:41:32 +0100

    Arduino code comments

--- Arduino/digipedal/digipedal.ino
+++ Arduino/digipedal/digipedal.ino
@@ -17,6 +17,17 @@ constexpr unsigned int brightLED { 255 };
 
 //   How many times around the main loop before LED goes dim
 constexpr unsigned int flashLEDDelay { 800 };
+
+//// A class to preprocess raw pedal vaules before the app sees them.
+//   The constructor takes two values:
+//     * the number of analog reads to average
+//     * the minimum interesting change
+//   The first is supposed to get rid of jitter-type noise,
+//   and the second stops mechanical vibration causing false
+//   triggering. Values of 4 and 3 seem to work OK.
+//
+//   Calling the object's process(val) method returns a cooked value
+//   given a raw one.
 class SampleProcessor {
   protected:
     int *readBuf;
@@ -63,20 +74,28 @@ void setup() {
 void loop() {
   static unsigned int result;
   static unsigned int dimLEDCounter;
-  
-  int potVal { analogRead(potPin) };
-  unsigned int newVal { sp.process(potVal) };
+
+  // Read a new pedal position value and process it.
+  unsigned int newVal { sp.process(analogRead(potPin)) };
+
+  // Do we need to report a change?
   if (newVal != result) {
+    // If so, remember the new value,
     result = newVal;
+    //   ... make the LED go bright for a bit
     analogWrite(LEDPin, brightLED);
     dimLEDCounter = flashLEDDelay;
+    //  ... and send the data normalised in the range 0..1
+    //      (four sig fig should be enough)
     Serial.println(result/1023.0, 4);
   }
 
+  // Dim the external LED when it's time.
   if (dimLEDCounter == 1) {
     analogWrite(LEDPin, dimLED);
   }
   if (dimLEDCounter) {
+    // Don't decrement past 0!
     --dimLEDCounter;
   }
 }
