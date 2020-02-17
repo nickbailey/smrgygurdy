8969279cb964020a99d054e5cd92d7d9e59a0937
Nick Bailey <nick@n-ism.org>
2020-02-17 19:55:27 +0000

    Sustain logic added (needs testing)

--- src/Keyboard.cpp
+++ src/Keyboard.cpp
@@ -4,151 +4,177 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "Controller.h"
+#include <set>
 #include <alsa/asoundlib.h>
 
 
 Keyboard::Keyboard(Controller *controller, int keyId, int keyPort, int verbosity) :
-  announce(verbosity > 0)
+announce(verbosity > 0)
 {
 
-  this->controller = controller;
-  this->keyboardId = keyId;
-  this->keyboardPort = keyPort;
-  
-  createSequencer();
-  connectKeyboard();
+	this->controller = controller;
+	this->keyboardId = keyId;
+	this->keyboardPort = keyPort;
+
+	createSequencer();
+	connectKeyboard();
 }
 
 
 
 void Keyboard::run() {
-	
-  int count;
-  struct pollfd* descriptors;
-  
-  count = snd_seq_poll_descriptors_count(sequencer, POLLIN);
-  descriptors = (struct pollfd *)alloca(count * sizeof(struct pollfd));
-  snd_seq_poll_descriptors(sequencer, descriptors, count, POLLIN);
+		
+	int count;
+	struct pollfd* descriptors;
 
-  while (1) {
-    if (poll(descriptors, count, 100000) > 0){
-      midiAction();
-    }  
-  }
+	count = snd_seq_poll_descriptors_count(sequencer, POLLIN);
+	descriptors = (struct pollfd *)alloca(count * sizeof(struct pollfd));
+	snd_seq_poll_descriptors(sequencer, descriptors, count, POLLIN);
 
-  free(descriptors);
-  
+	while (1) {
+		if (poll(descriptors, count, 100000) > 0){
+			midiAction();
+		}  
+	}
+
+	free(descriptors);
+
 }
 
 
 void Keyboard::createSequencer() {
 
-  if (snd_seq_open(&sequencer, "default", SND_SEQ_OPEN_INPUT, 0) < 0) {
-    fprintf(stderr, "Error opening ALSA sequencer.\n");
-    exit(1);
-  }
-  snd_seq_set_client_name(sequencer, "SMRGyGurdy");
-  if ((this->myPort = snd_seq_create_simple_port(sequencer, "Default Port",
-            SND_SEQ_PORT_CAP_WRITE|SND_SEQ_PORT_CAP_SUBS_WRITE,
-            SND_SEQ_PORT_TYPE_APPLICATION)) < 0) {
-    fprintf(stderr, "Error creating sequencer port.\n");
-    exit(1);
-  }
+	if (snd_seq_open(&sequencer, "default", SND_SEQ_OPEN_INPUT, 0) < 0) {
+		fprintf(stderr, "Error opening ALSA sequencer.\n");
+		exit(1);
+	}
+	snd_seq_set_client_name(sequencer, "SMRGyGurdy");
+	if ((this->myPort = snd_seq_create_simple_port(sequencer, "Default Port",
+			SND_SEQ_PORT_CAP_WRITE|SND_SEQ_PORT_CAP_SUBS_WRITE,
+			SND_SEQ_PORT_TYPE_APPLICATION)) < 0) {
+		fprintf(stderr, "Error creating sequencer port.\n");
+		exit(1);
+	}
 
-  this->myId = snd_seq_client_id(sequencer);
+	this->myId = snd_seq_client_id(sequencer);
 
 }
 
 
 void Keyboard::connectKeyboard()
 {
-  /*
-   *NOTE: myId, myPort, keyboardId, keyboardPort have to be defined before calling this function
-   */	
+/*
+*NOTE: myId, myPort, keyboardId, keyboardPort have to be defined before calling this function
+*/	
 
-  snd_seq_addr_t sender, dest;
-  snd_seq_port_subscribe_t *subs;
-  
-  //set sender to the keyboard client
-  sender.client = this->keyboardId;
-  sender.port = this->keyboardPort;
+	snd_seq_addr_t sender, dest;
+	snd_seq_port_subscribe_t *subs;
 
-  //set destination to the application client
-  dest.client = this->myId;
-  dest.port = this->myPort;
+	//set sender to the keyboard client
+	sender.client = this->keyboardId;
+	sender.port = this->keyboardPort;
 
-  //set the application client to receive data from the keyboard client
-  snd_seq_port_subscribe_alloca(&subs);
-  snd_seq_port_subscribe_set_sender(subs, &sender);
-  snd_seq_port_subscribe_set_dest(subs, &dest);
-  snd_seq_subscribe_port(sequencer, subs);
+	//set destination to the application client
+	dest.client = this->myId;
+	dest.port = this->myPort;
+
+	//set the application client to receive data from the keyboard client
+	snd_seq_port_subscribe_alloca(&subs);
+	snd_seq_port_subscribe_set_sender(subs, &sender);
+	snd_seq_port_subscribe_set_dest(subs, &dest);
+	snd_seq_subscribe_port(sequencer, subs);
 }
+
+void Keyboard::noteOn(unsigned char note,  unsigned char velocity) {
+	//  Send a note off if already sounding
+	if (deferred_noteoff.find(note) !=  deferred_noteoff.end())
+		controller->keyEvent(true, note);
+	
+	//  sound the note (velocity is ignored)
+	controller->keyEvent(false, note);
+	
+	if (announce)
+		fprintf(stderr, "Note On %d with velocity %d \n", note, velocity);
+}
+
+void Keyboard::noteOff(unsigned char note) {
+	//  If the sustain pedal is depressed,  the event is deferred
+	if (sustain) {
+		deferred_noteoff.insert(note);
+	} else {
+		controller->keyEvent(true, note);
+	}
+
+	if (announce && note != 0)
+		fprintf(stderr, "Note Off %d %s\n",
+			note,  sustain ? "(deferred)" : "");	
+}
+
+void Keyboard::release(void) {
+	for (keyset::iterator it = deferred_noteoff.begin(); it != deferred_noteoff.end(); ++it)
+		noteOff(*it);
+	deferred_noteoff.clear();
+ }
 
 
 void Keyboard::midiAction() {
 
-  snd_seq_event_t *event;
-  unsigned char note;
-  unsigned char velocity;
-  
-  do {
-    snd_seq_event_input(sequencer, &event);
-	
-	switch (event->type) {
-    case SND_SEQ_EVENT_NOTEON:
-		note =  event->data.note.note;
-		velocity = event->data.note.velocity;
-      
-		if (velocity != 0){
-			controller->keyEvent(false,note);
-			if (announce) {
-				fprintf(stderr, "Note On %d with velocity %d \n",note,velocity);
-			}
-		} else {
-			controller->keyEvent(true,note);
-			if (announce && note != 0) {
-				fprintf(stderr, "Note Off %d \n",note);
-			}
-		}
-	break; 
-       
-    case SND_SEQ_EVENT_NOTEOFF:
-		note =  event->data.note.note;
-		velocity = event->data.note.velocity;
+	snd_seq_event_t *event;
+	unsigned char note;
+	unsigned char velocity;
 
+	do {
+		snd_seq_event_input(sequencer, &event);
 		
-		controller->keyEvent(true,note);
-		if (announce && note != 0) {
-			fprintf(stderr, "Note Off %d \n",note); 
-        	}
-	break;
+		switch (event->type) {
+		case SND_SEQ_EVENT_NOTEON:
+			note =  event->data.note.note;
+			velocity = event->data.note.velocity;
 
-    case SND_SEQ_EVENT_CONTROLLER:
-        
-    	const int p = event->data.control.value;
+			if (velocity != 0)
+				noteOn(note,  velocity);
+			else
+				noteOff(note);
+			
+			break; 
 
-        switch (event->data.control.param) {
-        case 1:
-    		// Forward modulation events to the controller,
-    		// just in case anybody is interested.
-    		controller->modulationEvent(p);
-            
-            break;
+		case SND_SEQ_EVENT_NOTEOFF:
+			note =  event->data.note.note;
+			velocity = event->data.note.velocity;
+			
+			noteOff(note);
+			break;
 
-        case 64:
-            // Process sustain pedal events
-            if (announce) {
-                fprintf(stderr, "Sustain pedal change, value %d\n", p);
-            }
+		case SND_SEQ_EVENT_CONTROLLER:
+			
+			const int p = event->data.control.value;
 
-            break;
+			switch (event->data.control.param) {
+			case 1:
+				// Forward modulation events to the controller,
+				// just in case anybody is interested.
+				controller->modulationEvent(p);
+				break;
 
-        }
-        
-        break;
+			case 64:
+				// Process sustain pedal events
+				bool s {p > 63};
+				if (sustain && !s) release();
+				sustain = s;
+				
+				if (announce) {
+					fprintf(stderr, "Sustain pedal change, value %d (sustain %s)",
+							p,  this->sustain ? "on" : "off");
+				}
+				break;
 
-	}
-    snd_seq_free_event(event);
-  } while (snd_seq_event_input_pending(sequencer, 0) > 0);
+			}
+		
+			break;
+
+		}
+	
+	snd_seq_free_event(event);
+	} while (snd_seq_event_input_pending(sequencer, 0) > 0);
 
 }
--- src/Keyboard.h
+++ src/Keyboard.h
@@ -4,18 +4,18 @@
 #include "Thread.h"
 #include "Lock.h"
 #include "Controller.h"
-#include <list>
+#include <set>
 #include <alsa/asoundlib.h>
 
 /**
- * Provides interface for the ALSA Sequencer API.
- * The sequencer client subscribes to a USB keyboard client with a specified id
- * and polls it for incoming MIDI events. It passes the appropriate commands
- * to an instance of Controller based on what it receives.
- */
+* Provides interface for the ALSA Sequencer API.
+* The sequencer client subscribes to a USB keyboard client with a specified id
+* and polls it for incoming MIDI events. It passes the appropriate commands
+* to an instance of Controller based on what it receives.
+*/
 
 class Keyboard : public Thread {
-  
+
 	Controller* controller;
 	snd_seq_t* sequencer;
 	int myId;
@@ -23,45 +23,70 @@ class Keyboard : public Thread {
 	int keyboardId;
 	int keyboardPort;
 	int announce;
-    /** A list of note off commands not yet sent
-     *  because the sustain pedal is pressed.
-     */
-    std::list<int> deferred_noteoff;
-    bool sustain; /**< whether sustain pedal is depressed */
+	typedef std::set<int> keyset;
+	/** A list of note off commands not yet sent
+	*  because the sustain pedal is pressed.
+	*/
+	keyset deferred_noteoff;
+	bool sustain; /**< whether sustain pedal is depressed */
+	
+	/**
+     * Send a MIDI note on event,  taking the sustain pedal into account
+	 * 
+	 * If a note is already sounding due to sustain and a note-on message
+	 * is received,  convention dictates the synth should receive a note-off first.
+	 * Some synths misbehave if they receive two note-ons on the same channel.
+	 * 
+	 * @param note The MIDI note number.
+	 * @param velocity The MIDI note velocity (non-zero).
+	 */
+	void noteOn(unsigned char note,  unsigned char velocity);
 
 	/**
-	 * Creates a new ALSA sequencer client
-	 */		
+     * Send a MIDI note on event taking the sustain pedal into account (@sa noteOn)
+	 * 
+	 * @param note MIDI note number 
+     */
+	void noteOff(unsigned char note);
+	
+	/**
+     * Release all systained notes. @sa noteOn
+     */
+	void release(void);
+	
+	/**
+	* Creates a new ALSA sequencer client
+	*/		
 	void createSequencer();
 
 
 	/**
-	 * Handle a MIDI event
-	 *
-	 * Read a MIDI event and takes an appropriate action. Possible events:
-	 * CONTROLLER, PITCHBEND, NOTEON, NOTEOFF
-	 *
-	 * @param seq_handle pointer to the sequencer handle
-	 */
+	* Handle a MIDI event
+	*
+	* Read a MIDI event and takes an appropriate action. Possible events:
+	* CONTROLLER, PITCHBEND, NOTEON, NOTEOFF
+	*
+	* @param seq_handle pointer to the sequencer handle
+	*/
 	void midiAction();
 
 	void connectKeyboard();
 
 	public:
-  
+
 		/**
-		 * Default constructor
-		 * @param controller An instance of Controller class
-		 * @param keyId ALSA sequencer client id of the keyboard
-		 * @param keyPort Port number on the given client
-		 * @param verbosity Print note on/off announcement if > 0
-		 */
+		* Default constructor
+		* @param controller An instance of Controller class
+		* @param keyId ALSA sequencer client id of the keyboard
+		* @param keyPort Port number on the given client
+		* @param verbosity Print note on/off announcement if > 0
+		*/
 		Keyboard(Controller* controller, int keyId, int keyPort,
-		         int verbosity = 1);
+				int verbosity = 1);
 		
 		/**
-		 * Poll the MIDI keyboard for key events.
-		 */
+		* Poll the MIDI keyboard for key events.
+		*/
 		virtual void run();
 
 };
